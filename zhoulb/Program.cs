a29c968063fc77ea10e24bdaa85d200dc741dae3
liuww <liuww@SHCYF09N>
2017-10-17 14:19:18 +0800

    first

--- zhoulb/Program.cs
+++ zhoulb/Program.cs
@@ -14,6 +14,7 @@ namespace zhoulb
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
         }
     }
 }
